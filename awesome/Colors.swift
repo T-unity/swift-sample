abb9239c7bb0178048b20286e1362fa6a61b2b07
T-unity <takadanaritoshi@gmail.com>
2022-10-16 21:06:20 +0900

    ini

---
+++ awesome/Colors.swift
@@ -0,0 +1,17 @@
+/* https://satoriku.com/swift-color/ */
+
+import UIKit
+
+struct Colors {
+    let red = UIColor.red
+    let black = UIColor.black
+    let darkGray = UIColor.darkGray
+    let gray = UIColor.gray
+    let lightGray = UIColor.lightGray
+    let white = UIColor.white
+    let green = UIColor.green
+    let blue = UIColor.blue
+    let cyan = UIColor.cyan
+    let yellow = UIColor.yellow
+    let brown = UIColor.brown
+}
--- awesome/ViewController.swift
+++ awesome/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colors = Colors()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpGradation()
+        
+        let contentView = UIView()
+        let cvLayer = contentView.layer
+        
+        contentView.frame.size = CGSize(width: view.frame.size.width, height: 340)
+        contentView.center = CGPoint(x: view.center.x, y: view.center.y)
+        contentView.backgroundColor = .white
+        cvLayer.cornerRadius = 30
+        cvLayer.shadowOffset = CGSize(width: 2, height: 2)
+        cvLayer.shadowOpacity = 0.5
+        view.addSubview(contentView)
+    }
+    
+    func setUpGradation() {
+        let gLayer = CAGradientLayer()
+        // let view = view.frame.size
+        gLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 2)
+        gLayer.colors = [colors.blue.cgColor, colors.green.cgColor] // UIColorを.cgColorに型変換している。
+        gLayer.startPoint = CGPoint.init(x: 0, y: 0)
+        gLayer.endPoint = CGPoint.init(x: 1, y: 1)
+        view.layer.insertSublayer(gLayer, at: 0)
     }
 
-
 }
-
