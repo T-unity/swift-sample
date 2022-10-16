ae704b789eba6bba41ea0f202e16449de0e0c236
T-unity <takadanaritoshi@gmail.com>
2022-10-16 23:07:44 +0900

    それっぽいテキストを表示

--- awesome/ViewController.swift
+++ awesome/ViewController.swift
@@ -15,19 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setUpGradation()
-        
-        let contentView = UIView()
-        let cvLayer = contentView.layer
-        
-        contentView.frame.size = CGSize(width: view.frame.size.width, height: 340)
-        contentView.center = CGPoint(x: view.center.x, y: view.center.y)
-        contentView.backgroundColor = .white
-        cvLayer.cornerRadius = 30
-        cvLayer.shadowOffset = CGSize(width: 2, height: 2)
-        cvLayer.shadowOpacity = 0.5
-        view.addSubview(contentView)
+        setUpContent()
     }
-    
+
     func setUpGradation() {
         let gLayer = CAGradientLayer()
         // let view = view.frame.size
@@ -38,4 +28,41 @@ class ViewController: UIViewController {
         view.layer.insertSublayer(gLayer, at: 0)
     }
 
+    func setUpContent() {
+        let contentView = UIView()
+        let cvLayer = contentView.layer
+        
+        contentView.frame.size = CGSize(width: view.frame.size.width, height: 340)
+        contentView.center = CGPoint(x: view.center.x, y: view.center.y)
+        contentView.backgroundColor = .white
+        cvLayer.cornerRadius = 30
+        cvLayer.shadowOffset = CGSize(width: 2, height: 2)
+        cvLayer.shadowOpacity = 0.5
+        
+        contentView.layer.opacity = 0.8
+        view.addSubview(contentView)
+        
+        view.backgroundColor = .systemGray6
+        
+        let labelfont = UIFont.systemFont(ofSize: 15, weight: .heavy)
+        let size = CGSize(width: 150, height: 50)
+        let color = colors.bluePurple
+        let leftX = view.frame.size.width * 0.33
+        let rightX = view.frame.size.width * 0.80
+        
+        setUpLabel("item 1", size: size, centerX: leftX, y: 20, font: labelfont, color: color, contentView)
+        setUpLabel("item 2", size: size, centerX: rightX, y: 20, font: labelfont, color: color, contentView)
+    }
+    
+    func setUpLabel(_ text: String, size: CGSize, centerX: CGFloat, y: CGFloat, font: UIFont, color: UIColor, _ parentView: UIView) {
+        let label = UILabel()
+        
+        label.text = text
+        label.frame.size = size
+        label.center.x = centerX
+        label.frame.origin.y = y
+        label.font = font
+        label.textColor = color
+        parentView.addSubview(label)
+    }
 }
